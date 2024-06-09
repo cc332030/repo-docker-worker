d0aad8ec9c84569005745721ab97275544a0026c
c332030 <c332030@c332030.com>
2024-06-09 11:21:11 +0800

    feat: default

--- src/index.js
+++ src/index.js
@@ -1,189 +1,80 @@
-'use strict'
 
-const hub_host = 'registry-1.docker.io'
-const auth_url = 'https://auth.docker.io'
-const workers_url = 'https://docker.c332030.com'
-/**
- * static files (404.html, sw.js, conf.js)
- */
+const repos = [
 
-/** @type {RequestInit} */
-const PREFLIGHT_INIT = {
-  status: 204,
-  headers: new Headers({
-    'access-control-allow-origin': '*',
-    'access-control-allow-methods': 'GET,POST,PUT,PATCH,TRACE,DELETE,HEAD,OPTIONS',
-    'access-control-max-age': '1728000',
-  }),
+  'https://docker.io',
+
+]
+
+const hiddenErrorCodes = [
+  401
+]
+
+const urlPrefix = '://'
+
+function getPath(url) {
+
+  const url2 = url.substring(url.indexOf(urlPrefix) + urlPrefix.length)
+  return url2.substring(url2.indexOf('/'))
 }
 
-/**
- * @param {any} body
- * @param {number} status
- * @param {Object<string, string>} headers
- */
-function makeRes(body, status = 200, headers = {}) {
-  headers['access-control-allow-origin'] = '*'
-  return new Response(body, {status, headers})
+function assign(obj1, obj2) {
+  return Object.assign({}, obj1, obj2)
 }
 
+export default {
+  async fetch(request, env, ctx) {
 
-/**
- * @param {string} urlStr
- */
-function newUrl(urlStr) {
-  try {
-    return new URL(urlStr)
-  } catch (err) {
-    return null
-  }
-}
+    const path = getPath(request.url)
 
+    const failResponses = []
+    for (const repo of repos) {
 
-addEventListener('fetch', e => {
-  const ret = fetchHandler(e)
-    .catch(err => makeRes('cfworker error:\n' + err.stack, 502))
-  e.respondWith(ret)
-})
+      const newUrl = `${repo}${path}`
+      console.debug('newUrl', newUrl)
 
+      const requestHeaders = request.headers
+      console.debug('headers', requestHeaders)
 
-/**
- * @param {FetchEvent} e
- */
-async function fetchHandler(e) {
-  const getReqHeader = (key) => e.request.headers.get(key);
-
-  let url = new URL(e.request.url);
-
-  if (url.pathname === '/token') {
-    let token_parameter = {
-      headers: {
-        'Host': 'auth.docker.io',
-        'User-Agent': getReqHeader("User-Agent"),
-        'Accept': getReqHeader("Accept"),
-        'Accept-Language': getReqHeader("Accept-Language"),
-        'Accept-Encoding': getReqHeader("Accept-Encoding"),
-        'Connection': 'keep-alive',
-        'Cache-Control': 'max-age=0'
-      }
-    };
-    let token_url = auth_url + url.pathname + url.search
-    return fetch(new Request(token_url, e.request), token_parameter)
-  }
-
-  url.hostname = hub_host;
-
-  let parameter = {
-    headers: {
-      'Host': hub_host,
-      'User-Agent': getReqHeader("User-Agent"),
-      'Accept': getReqHeader("Accept"),
-      'Accept-Language': getReqHeader("Accept-Language"),
-      'Accept-Encoding': getReqHeader("Accept-Encoding"),
-      'Connection': 'keep-alive',
-      'Cache-Control': 'max-age=0'
-    },
-    cacheTtl: 3600
-  };
-
-  if (e.request.headers.has("Authorization")) {
-    parameter.headers.Authorization = getReqHeader("Authorization");
-  }
-
-  let original_response = await fetch(new Request(url, e.request), parameter)
-  let original_response_clone = original_response.clone();
-  let original_text = original_response_clone.body;
-  let response_headers = original_response.headers;
-  let new_response_headers = new Headers(response_headers);
-  let status = original_response.status;
-
-  if (new_response_headers.get("Www-Authenticate")) {
-    let auth = new_response_headers.get("Www-Authenticate");
-    let re = new RegExp(auth_url, 'g');
-    new_response_headers.set("Www-Authenticate", response_headers.get("Www-Authenticate").replace(re, workers_url));
-  }
-
-  if (new_response_headers.get("Location")) {
-    return httpHandler(e.request, new_response_headers.get("Location"))
-  }
-
-  let response = new Response(original_text, {
-    status,
-    headers: new_response_headers
-  })
-  return response;
-
-}
-
-
-/**
- * @param {Request} req
- * @param {string} pathname
- */
-function httpHandler(req, pathname) {
-  const reqHdrRaw = req.headers
-
-  // preflight
-  if (req.method === 'OPTIONS' &&
-    reqHdrRaw.has('access-control-request-headers')
-  ) {
-    return new Response(null, PREFLIGHT_INIT)
-  }
-
-  let rawLen = ''
-
-  const reqHdrNew = new Headers(reqHdrRaw)
-
-  const refer = reqHdrNew.get('referer')
-
-  let urlStr = pathname
-
-  const urlObj = newUrl(urlStr)
-
-  /** @type {RequestInit} */
-  const reqInit = {
-    method: req.method,
-    headers: reqHdrNew,
-    redirect: 'follow',
-    body: req.body
-  }
-  return proxy(urlObj, reqInit, rawLen, 0)
-}
-
-
-/**
- *
- * @param {URL} urlObj
- * @param {RequestInit} reqInit
- */
-async function proxy(urlObj, reqInit, rawLen) {
-  const res = await fetch(urlObj.href, reqInit)
-  const resHdrOld = res.headers
-  const resHdrNew = new Headers(resHdrOld)
-
-  // verify
-  if (rawLen) {
-    const newLen = resHdrOld.get('content-length') || ''
-    const badLen = (rawLen !== newLen)
-
-    if (badLen) {
-      return makeRes(res.body, 400, {
-        '--error': `bad len: ${newLen}, except: ${rawLen}`,
-        'access-control-expose-headers': '--error',
+      const response = await fetch(newUrl, {
+        method: request.method,
+        redirect: 'follow',
+        headers: requestHeaders,
       })
+      console.debug('response', response)
+
+      const ok = response.ok
+      console.debug('ok', ok)
+      if(ok) {
+
+        const responseHeaders = {
+          'c-repo': repo,
+          'c-url': response.url,
+        }
+
+        for (const pair of response.headers.entries()) {
+          responseHeaders[pair[0]] = pair[1]
+        }
+
+        return new Response(response.body, {
+          status: response.status,
+          statusText: response.statusText,
+          headers: responseHeaders
+        })
+      }
+
+      failResponses.push(response)
+
     }
+
+    for (const res of failResponses) {
+      if(hiddenErrorCodes.includes(res.status)) {
+        continue
+      }
+      return res;
+    }
+
+    return new Response(`${path} not found.`, {
+      status: 404,
+    });
   }
-  const status = res.status
-  resHdrNew.set('access-control-expose-headers', '*')
-  resHdrNew.set('access-control-allow-origin', '*')
-  resHdrNew.set('Cache-Control', 'max-age=1500')
-
-  resHdrNew.delete('content-security-policy')
-  resHdrNew.delete('content-security-policy-report-only')
-  resHdrNew.delete('clear-site-data')
-
-  return new Response(res.body, {
-    status,
-    headers: resHdrNew
-  })
-}
+};
